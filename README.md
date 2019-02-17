0e9f5769110c745293e9f32c4c52f628de293f15
Marcelo Aguila <web@croop.cl>
2019-02-17 16:02:49 -0300

    First day, first commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # day_01
-#100DaysOfCoding
+### 100DaysOfCoding
+
+## Challenge
+
+Creating a todo-list app with Vue
+
+## Funcionalities
+
+* Create tasks
+* Delete tasks
+* Show how long task was created 
+
+## Tools used
+
+* Vuejs
+* Momentjs
+* Bulma
+* fontawesome
+* Eslint for linting
+
+## Learned
+
+First time using vue, moment & bulma (i liked bulma a lot btw)
---
+++ app.js
@@ -0,0 +1,35 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    task: null,
+    todos: []
+  },
+  methods: {
+    addTask: function (task) {
+      var date = moment().format("YYYY/MM/DD HH:mm:ss")
+      if (task) {
+        var newTask = {
+          task: task,
+          createdAt: date
+        }
+        this.todos.push(newTask)
+        this.task = ''
+        date = null
+      }
+    },
+    deleteTask: function (t) {
+      this.todos.splice(t, 1)
+    }
+  },
+  filters: {
+    fromNow (date) {
+      return moment(date, "YYYY/MM/DD HH:mm:ss").fromNow()
+    }
+  },
+  created () {
+    this.interval = setInterval(() => this.$forceUpdate(), 5000)
+  },
+  beforeDestroy () {
+    clearInterval(this.interval)
+  }
+})
